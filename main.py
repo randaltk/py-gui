4d77d223050164337b4ff4cd6004c1f142c37c1c
Randal Krebs <randal.t.krebs@gmail.com>
2023-09-14 21:55:39 -0300

    feat:create structure

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('okasd')
+import tkinter as tk
+from pytube import YouTube
+
+app = tk.Tk()
+app.geometry("720x480")
+app.title("Youtube Downloader")
+
+
+download_button = tk.Button(app, text="Download")
+
+status_label = tk.Label(app, text="")
+status_label.pack()
+
+
+app.mainloop()
